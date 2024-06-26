63ae9b976c103c786e458a59f0a8a70048cb7644
yahiro-1120 <ec2-user@ip-172-31-35-24.ap-northeast-1.compute.internal>
2024-06-26 12:14:12 +0000

    フェーズ１

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,10 +10,22 @@ tom = User.find_or_create_by!(email: "tom@example.com") do |user|
   user.name = "Tom"
   user.password = "password"
   user.profile_image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-user1.jpg"), filename:"sample-user1.jpg")
-end 
+end
+
+jun = User.find_or_create_by!(email: "jun@example.com") do |user|
+  user.name = "Jun"
+  user.password = "password"
+  user.profile_image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-user2.jpg"), filename:"sample-user2.jpg")
+end
 
 Post.find_or_create_by!(pref: "岐阜県") do |post|
   post.title = "日帰り"
   post.body = "11時飛騨・高山、１２時お昼ごはん"
   post.user = tom
+end
+
+Post.find_or_create_by!(pref: "愛知県") do |post|
+  post.title = "泊まり"
+  post.body = "１７時チェックイン、９時チェックアウト"
+  post.user = jun
 end
