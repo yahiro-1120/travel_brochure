ed94a00b62824d674447a7723a5ba4043d2ef310
yahiro-1120 <ec2-user@ip-172-31-35-24.ap-northeast-1.compute.internal>
2024-07-03 10:00:16 +0000

    管理者機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,32 @@
 Rails.application.routes.draw do
-  get 'users/show'
-  get 'users/edit'
-  devise_for :users
-  devise_scope :user do
-    post "users/guest_sign_in", to: "users/sessions#guest_sign_in"
+  scope module: :public do
+    get 'users/show'
+    get 'users/edit'
+    devise_for :users
+    devise_scope :user do
+      post "users/guest_sign_in", to: "users/sessions#guest_sign_in"
+    end
+
+    root to: "homes#top"
+    get 'homes/about'
+
+    resources :posts do
+      resources :post_comments, only: [:create, :destroy]
+    end
+
+    resources :users
+    get '/search', to: 'searches#search'
   end
 
-  root to: "homes#top"
-  get 'homes/about'
 
-  resources :posts do
-    resources :post_comments, only: [:create, :destroy]
+
+  devise_for :admin, skip: [:registrations, :password], controllers: {
+    sessions: 'admin/sessions'
+  }
+
+  namespace :admin do
+    get 'dashboards', to: 'dashboards#index'
+    resources :users, only: [:destroy]
   end
-
-  resources :users
-  get '/search', to: 'searches#search'
-
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
