bec4683daa6c5884174bfce2b3c813a165257222
yahiro-1120 <ec2-user@ip-172-31-35-24.ap-northeast-1.compute.internal>
2024-07-10 07:24:12 +0000

    コメント機能

--- app/controllers/public/post_comments_controller.rb
+++ app/controllers/public/post_comments_controller.rb
@@ -4,8 +4,11 @@ class Public::PostCommentsController < ApplicationController
     post = Post.find(params[:post_id])
     comment = current_user.post_comments.new(post_comment_params)
     comment.post_id = post.id
-    comment.save
-    redirect_to post_path(post)
+    if comment.save
+      redirect_to post_path(post)
+    else
+      redirect_to post_path(post)
+    end
   end
 
   def destroy
--- app/models/post_comment.rb
+++ app/models/post_comment.rb
@@ -3,4 +3,6 @@ class PostComment < ApplicationRecord
   belongs_to :user
   belongs_to :post
 
+  validates :body, presence: true
+
 end
