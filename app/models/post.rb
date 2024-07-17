4bbe80ee820400e3a4f08295fe0f2b4b9f489869
yahiro-1120 <ec2-user@ip-172-31-35-24.ap-northeast-1.compute.internal>
2024-07-17 08:57:56 +0000

    ブックマーク機能

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,10 +3,15 @@ class Post < ApplicationRecord
   has_one_attached :image
   belongs_to :user
   has_many :post_comments, dependent: :destroy
+  has_many :bookmarks, dependent: :destroy
 
   validates :pref, presence: true
   validates :title, presence: true
   validates :body, presence: true
+
+  def bookmarked_by?(user)
+    bookmarks.exists?(user_id: user.id)
+  end
 
   def self.search_for(content, method)
     if method == 'perfect'
